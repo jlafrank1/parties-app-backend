810c2961f7ce15dfe04f4119a67cefd6372adeba
Jess LaFrank <jesslafrank@gmail.com>
2021-09-30 09:22:29 -0700

    Added INDEX route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 9000
 
+const partiesController = require('./controllers/parties.js')
+
+app.use('/parties', partiesController)
+
 app.listen(PORT, () => {
   console.log('🎉🎊', `celebrations happening on port ${PORT}`, '🎉🎊',)
 })
